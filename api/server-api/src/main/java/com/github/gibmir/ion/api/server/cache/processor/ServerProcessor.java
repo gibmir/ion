b29439152f222fc2dda707757d2950fcf030f7f3
pavel <pavel.usachev1@gmail.com>
2020-07-21 23:51:14 +0300

    Add raw batch processing

--- api/server-api/src/main/java/com/github/gibmir/ion/api/server/cache/processor/ServerProcessor.java
+++ api/server-api/src/main/java/com/github/gibmir/ion/api/server/cache/processor/ServerProcessor.java
@@ -2,15 +2,18 @@ package com.github.gibmir.ion.api.server.cache.processor;
 
 import com.github.gibmir.ion.api.dto.properties.SerializationProperties;
 import com.github.gibmir.ion.api.dto.response.JsonRpcResponse;
+import com.github.gibmir.ion.api.dto.response.transfer.batch.BatchResponseDto;
 import com.github.gibmir.ion.api.dto.response.transfer.error.ErrorResponse;
 import com.github.gibmir.ion.api.dto.response.transfer.error.Errors;
 import com.github.gibmir.ion.api.dto.response.transfer.error.JsonRpcError;
 
+import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonString;
-import javax.json.JsonStructure;
 import javax.json.JsonValue;
 import javax.json.bind.Jsonb;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ServerProcessor {
@@ -20,15 +23,30 @@ public class ServerProcessor {
     this.procedureProcessorRegistry = procedureProcessorRegistry;
   }
 
-  public void process(JsonStructure jsonStructure, Jsonb jsonb, Consumer<JsonRpcResponse> responseConsumer) {
+  public void process(JsonValue jsonStructure, Jsonb jsonb, Consumer<JsonRpcResponse> responseConsumer) {
     switch (jsonStructure.getValueType()) {
       case OBJECT:
         processObject((JsonObject) jsonStructure, jsonb, responseConsumer);
         return;
       case ARRAY:
+        processBatch((JsonArray) jsonStructure, jsonb, responseConsumer);
+        return;
       default:
         responseConsumer.accept(ErrorResponse.withNullId(Errors.INVALID_RPC.getError()));
     }
+  }
+
+  public void processBatch(JsonArray jsonArray, Jsonb jsonb, Consumer<JsonRpcResponse> responseConsumer) {
+    int batchSize = jsonArray.size();
+    List<JsonRpcResponse> jsonRpcResponseList = new ArrayList<>(batchSize);
+    for (JsonValue jsonValue : jsonArray) {
+      try {
+        processObject((JsonObject) jsonValue, jsonb, jsonRpcResponseList::add);
+      } catch (Exception e) {
+        jsonRpcResponseList.add(ErrorResponse.withNullId(e));
+      }
+    }
+    responseConsumer.accept(new BatchResponseDto(jsonRpcResponseList.toArray(new JsonRpcResponse[batchSize])));
   }
 
   public void processObject(JsonObject jsonObject, Jsonb jsonb, Consumer<JsonRpcResponse> responseConsumer) {
